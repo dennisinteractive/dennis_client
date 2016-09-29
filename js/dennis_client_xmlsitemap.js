7bb662ba6e8d8f89f513c7a4d9bac09e71fbfd06
Sergio <guardiola86@gmail.com>
2016-09-29 16:51:15 +0100

    Case 25449;Update select on change

--- js/dennis_client_xmlsitemap.js
+++ js/dennis_client_xmlsitemap.js
@@ -5,10 +5,16 @@
         // Exclude the content from the xml sitemap if  a term in content partnerships has been selected.
         if (this.value != "_none") {
           $('#edit-xmlsitemap-status option[value=0]').prop('selected', 'selected');
+          status = 0;
         }
         else {
-          $('#edit-xmlsitemap-status option[value=1]').prop('selected', 'selected')
+          $('#edit-xmlsitemap-status option[value=1]').prop('selected', 'selected');
+          status = 1;
         }
+        // Remove all selected from options.
+        $("#edit-xmlsitemap-status option").attr('selected', false);
+        // Add select attribute to the chosen option.
+        $("#edit-xmlsitemap-status option[value=" + status + "]").attr('selected','selected');
       });
     }
   };
